b4d1a085ef92e19c8af8e43dd061fab6da4e576d
Erik <erik.grundy@gmail.com>
2021-12-01 01:53:21 +1300

    Rearranged, and gave collections of functions titles

--- src/Solving.hs
+++ src/Solving.hs
@@ -13,11 +13,15 @@ import qualified Data.Set as S
 import ArrayUtils
 import Types
 
+-- Cell level manipulations
+
 isFilledIn :: Cell -> Bool
 isFilledIn = isJust . number
 
 isBlank :: Cell -> Bool
 isBlank = isNothing . number
+
+-- Working with houses
 
 row :: Int -> [Position]
 row a = [(a, b) | b <- [0 .. 8]]
@@ -41,6 +45,8 @@ houses = rows ++ cols ++ boxes
     cols = map col [0 .. 8]
     boxes = [box (r, c) | r <- [0, 3, 6], c <- [0, 3, 6]]
 
+-- Utility functions
+
 updateNotes :: Grid -> Grid
 updateNotes grid = mapArray f grid
   where
@@ -48,15 +54,57 @@ updateNotes grid = mapArray f grid
       let others = mapMaybe (number . (grid !)) $ buddies pos
        in cell { notes = ns I.\\ I.fromList others }
 
+filterBlanks :: Grid -> [Position] -> [Position]
+filterBlanks grid = filter (isBlank . (grid !))
+
+subsets :: [a] -> [[a]]
+subsets = foldr (\x xs -> xs ++ map (x :) xs) [[]]
+
+acceptChange :: Change -> Grid -> Grid
+acceptChange (FillInNum pos n) grid = grid // [(pos, cell')]
+  where
+    cell = grid ! pos
+    cell' = cell {number = Just n}
+acceptChange (RemoveNote pos n) grid = grid // [(pos, cell')]
+  where
+    cell = grid ! pos
+    cell' = cell {notes = I.delete n $ notes cell}
+
+acceptRecommendation :: Recommendation -> Grid -> Grid
+acceptRecommendation changes grid = foldr acceptChange grid changes
+
+validChange :: Grid -> Change -> Bool
+validChange grid (FillInNum pos _) = isBlank $ grid ! pos
+validChange grid (RemoveNote pos n) = I.member n $ notes (grid ! pos)
+
+isSolved :: Grid -> Bool
+isSolved grid = all (valid . map (grid !)) houses
+  where
+    valid house =
+      all (isJust . number) house &&
+      length (nub house) == length house
+
+{-
+Continuously apply the recommender, until it either can't generate new
+recommendations, or it completes the grid. Returns (rs, grid), where rs is the
+list of changes made, and grid is as far as it got.
+-}
+solve :: Recommender -> Grid -> ([Recommendation], Grid)
+solve recommender = go []
+  where
+    go rs grid
+      | Just r <- recommender grid =
+        go (r : rs) $ updateNotes $ acceptRecommendation r grid
+      | otherwise = (reverse rs, grid)
+
+-- Recommenders
+
 -- Look, I don't come up with the names
 nakedSingle :: Recommender
 nakedSingle grid = mkChange <$> findArray isNakedSingle grid
   where
     mkChange (pos, Cell {notes = ns}) = [FillInNum pos (I.findMin ns)]
     isNakedSingle _ cell = isBlank cell && I.size (notes cell) == 1
-
-filterBlanks :: Grid -> [Position] -> [Position]
-filterBlanks grid = filter (isBlank . (grid !))
 
 hiddenSingle :: Recommender
 hiddenSingle grid =
@@ -68,13 +116,6 @@ hiddenSingle grid =
       _ -> return Nothing
   where
     hasNote n cell = I.member n $ notes cell
-
-subsets :: [a] -> [[a]]
-subsets = foldr (\x xs -> xs ++ map (x :) xs) [[]]
-
-validChange :: Grid -> Change -> Bool
-validChange grid (FillInNum pos _) = isBlank $ grid ! pos
-validChange grid (RemoveNote pos n) = I.member n $ notes (grid ! pos)
 
 nakedSubset :: Recommender
 nakedSubset grid =
@@ -100,36 +141,3 @@ nakedSubset grid =
 overallRecommender :: Recommender
 overallRecommender grid =
   asum $ map ($ grid) [nakedSingle, hiddenSingle, nakedSubset]
-
-acceptChange :: Change -> Grid -> Grid
-acceptChange (FillInNum pos n) grid = grid // [(pos, cell')]
-  where
-    cell = grid ! pos
-    cell' = cell {number = Just n}
-acceptChange (RemoveNote pos n) grid = grid // [(pos, cell')]
-  where
-    cell = grid ! pos
-    cell' = cell {notes = I.delete n $ notes cell}
-
-acceptRecommendation :: Recommendation -> Grid -> Grid
-acceptRecommendation changes grid = foldr acceptChange grid changes
-
-isSolved :: Grid -> Bool
-isSolved grid = all (valid . map (grid !)) houses
-  where
-    valid house =
-      all (isJust . number) house &&
-      length (nub house) == length house
-
-{-
-Continuously apply the recommender, until it either can't generate new
-recommendations, or it completes the grid. Returns (rs, grid), where rs is the
-list of changes made, and grid is as far as it got.
--}
-solve :: Recommender -> Grid -> ([Recommendation], Grid)
-solve recommender = go []
-  where
-    go rs grid
-      | Just r <- recommender grid =
-        go (r : rs) $ updateNotes $ acceptRecommendation r grid
-      | otherwise = (reverse rs, grid)
